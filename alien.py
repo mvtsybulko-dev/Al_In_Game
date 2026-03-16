fd753604f467c7add37a48b75878aebe481acdd0
NickTsib <tsibulko.itstep@gmail.com>
2026-03-16 15:24:43 +0200

    U8

--- alien.py
+++ alien.py
@@ -5,8 +5,12 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.image = pygame.image.load("alien.png")
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
         self.x = float(self.rect.x)
+
+    def update(self):
+        pass
