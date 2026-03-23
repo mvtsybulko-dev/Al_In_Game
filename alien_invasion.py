7661f8b51e33dff7a1d0bfd13caf6fc5d794565d
mvtsybulko-dev <m.v.tsybulko@gmail.com>
2026-03-23 15:38:19 +0200

    U9

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,7 +84,7 @@ class AlienInvasion:
 
     def _check_fleet_edges(self):
         for alien in self.aliens.sprites():
-            if alien.check_edges:
+            if alien.check_edges():
                 self._change_fleet_direction()
                 break
 
