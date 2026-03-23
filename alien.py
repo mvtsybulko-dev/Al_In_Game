29dc066f0dee00ca01828c51fba5c733cfb489d0
mvtsybulko-dev <m.v.tsybulko@gmail.com>
2026-03-23 15:31:52 +0200

    U9

--- alien.py
+++ alien.py
@@ -12,5 +12,12 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
         self.x = float(self.rect.x)
 
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
     def update(self):
-        pass
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+
