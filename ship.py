0d6c1a48509556581f064393e163b4204f9e165f
NickTsib <tsibulko.itstep@gmail.com>
2026-04-04 15:34:30 +0300

    U11

--- ship.py
+++ ship.py
@@ -12,8 +12,6 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
-
-
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
@@ -23,3 +21,7 @@ class Ship:
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         self.rect.x = self.x
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
