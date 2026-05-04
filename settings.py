7168ee9d40821984184a071823c8fdc4d0421856
NickTsib <tsibulko.itstep@gmail.com>
2026-05-04 15:17:07 +0300

    U15

--- settings.py
+++ settings.py
@@ -3,12 +3,13 @@ class Settings:
         self.bg_color = (25, 10, 105)
         self.screen_width = 1200
         self.screen_height = 800
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 210, 60)
         self.fleet_drop_speed = 10
         self.ship_limit = 3
         self.speedup = 1.2
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -16,8 +17,11 @@ class Settings:
         self.bullet_speed = 2
         self.alien_speed = 1.0
         self.fleet_direction = 1
+        self.alien_points = 50
 
     def increase_speed(self):
         self.ship_speed *= self.speedup
         self.bullet_speed *= self.speedup
         self.alien_speed *= self.speedup
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
